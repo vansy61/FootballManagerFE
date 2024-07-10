627dea7f94edf6ba5db9525e54ea750ac2481959
vansy61 <49103390+vansy61@users.noreply.github.com>
2024-07-10 08:52:38 +0700

    update tiny

--- js/menu.js
+++ js/menu.js
@@ -11,28 +11,28 @@ const showMenu = () => {
     ];
 
     const menu = `
-        <nav class="navbar navbar-expand-lg bg-white shadow-sm">
-          <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg bg-white shadow-sm">
+        <div class="container-fluid">
             <a class="navbar-brand" href="#">SC FOOTBALL FC</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainMenu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainMenu"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="mainMenu">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="menus">
-                <li class="nav-item">
-                  <a class="nav-link" href="../index.html">Trang Chủ</a>
-                </li>
-              </ul>
-              <div class="d-flex align-items-center">
-                 <span>Xin chào ${USER.username}</span>
-                 <button class="btn btn-link text-decoration-none" type="button" id="logout">
-                    <span class="me-2">Đăng Xuất</span>
-                    <i class="fa-solid fa-right-from-bracket"></i>
-                 </button>
-              </div>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="menus">
+                    <li class="nav-item"><a class="nav-link" href="../index.html">Trang Chủ</a>
+                    </li>
+                </ul>
+                <div class="d-flex align-items-center">
+                    <span>Xin chào ${USER.username}</span>
+                    <button class="btn btn-link text-decoration-none" type="button" id="logout">
+                        <span class="me-2">Đăng Xuất</span>
+                        <i class="fa-solid fa-right-from-bracket"></i>
+                    </button>
+                </div>
             </div>
-          </div>
-        </nav>
+        </div>
+    </nav>
     `;
 
     $("#menu").append(menu);
--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,14 @@
 const showPage = () => {
-    alert("Chức năng đang phát triển!");
+    $("#content").html(`
+        <div id="profile-content" class="mb-5 bg-white shadow-sm rounded p-3">
+            ${loaderTemplate()}
+        </div>
+        <div class="shadow-sm bg-white p-3 rounded">
+            <h5>Biểu đồ lương</h5>
+            <div id="salary-chart">
+                ${loaderTemplate()}
+            </div>
+        </div>
+    `)
+
 }
